60b1154319676b813a824bdfe4d05a58b59fb8b6
Ricky Barfield <barfieldfredrick@gmail.com>
2023-09-04 14:55:26 -0700

    starting to add JS, getting css to cooperate...

--- The Budding Chef/js/main.js
+++ The Budding Chef/js/main.js
@@ -1,8 +1,23 @@
-document.querySelector('button').addEventListener('click', getFetch())
+document.getElementById('ingredientButton').addEventListener('click', getFood)
+document.getElementById('recipeButton').addEventListener('click', cookDirections)
 
-function getFetch (){
-    // const foodSearch = document.querySelector('input').value
-    fetch('https:www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
+function getFood () {
+    let foodSearch = document.querySelector('input').value
+    console.log(foodSearch)
+    let resultNum = []
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${foodSearch}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById('resultNum').innerText = `${data.meals.length} Items Found!`
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
+
+function cookDirections () {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
     .then(res => res.json())
     .then(data => {
         console.log(data)
@@ -11,3 +26,18 @@ function getFetch (){
         console.log(`error ${err}`)
     })
 }
+
+//------for GET FOOD ----
+// All data is within an array called meals. 
+// strMeal = title of the meals
+// strMealThumb = url picture of the meal
+
+// -----for COOK DIRECTIONS -----
+// all data is within an array called meals
+// strMeal is the title of the meals
+
+// could have strIngredients1-20, empty elements are either null or ""...These appear to be matched with strMeasure1-20
+
+// strInstructions are the instructions for the dish
+
+// may have an attached youtube url video on strYoutube
