34d4a2eeb28b55d5614d7631f204f96fa903d1cc
Ricky Barfield <barfieldfredrick@gmail.com>
2023-09-05 17:38:08 -0700

    uploaded incorrect file. Iz better now

--- The Budding Chef/js/main.js
+++ The Budding Chef/js/main.js
@@ -1,6 +1,21 @@
 document.getElementById('ingredientButton').addEventListener('click', getFood)
 document.getElementById('recipeButton').addEventListener('click', cookDirections)
 
+function scrollChildCheck () {
+    let targetList = document.getElementById('recipeList')
+    while (targetList.firstChild) {
+        targetList.removeChild(targetList.lastChild)
+    }
+}
+
+function recipeIngredientChildCheck () {
+    let targetList = document.getElementById('recipeIngredients')
+    while (targetList.firstChild) {
+        targetList.removeChild(targetList.lastChild)
+    }
+}
+
+
 function getFood () {
     let foodSearch = document.querySelector('input').value
     console.log(foodSearch)
@@ -9,18 +24,61 @@ function getFood () {
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        document.getElementById('resultNum').innerText = `${data.meals.length} Items Found!`
-    })
+        scrollChildCheck()
+        if(data.meals === null) {
+            document.getElementById('resultNum').innerText = 'No Items found! Try again with a different food.'
+        } else {
+            document.getElementById('resultNum').innerText = `${data.meals.length} Items Found!`
+            for (let i = 0; i < data.meals.length; i++) {
+                let li = document.createElement('li')
+                let mealName = data.meals[i].strMeal
+                li.textContent += mealName
+                document.getElementById('recipeList').appendChild(li)
+            }
+        }
+        })
     .catch(err => {
         console.log(`error ${err}`)
     })
 }
 
+// video wont load. needs embed, not watch in link: text = text.replace("watch?v=", "embed/")
 function cookDirections () {
+    let h3 = document.getElementById('recipeTitle')
+    let ul = document.getElementById('recipeIngredients')
+    let p = document.getElementById('recipeDirections')
+
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        recipeIngredientChildCheck()
+        let youtubeLink = data.meals[0].strYoutube
+        youtubeLink = youtubeLink.replace("watch?v=", "embed/")
+        console.log(youtubeLink)
+
+        document.getElementById('midPic').src = data.meals[0].strMealThumb
+        document.getElementById('midVid').src = data.meals[0].strYoutube
+        if (data.meals[0] === null) {
+            document.getElementById('recipeTitle').innerText = 'Recipe Not Found! Try again.'
+        } else {
+            h3.innerText = data.meals[0].strMeal
+            p.innerText = data.meals[0].strInstructions
+            for (let i = 0; i < 21; i++) {
+                if (data.meals[i] === null || data.meals[i] === '') {
+                    continue;
+                } else if {
+                    let blah = document.createElement('li')
+                }
+                // let li2 = document.createElement('li')
+                // let mealName = data.meals[i].strMeal
+                // li.textContent += mealName
+                // document.getElementById('recipeIngredients').appendChild(li)
+                // }
+                
+            }
+
+        }
     })
     .catch(err => {
         console.log(`error ${err}`)
