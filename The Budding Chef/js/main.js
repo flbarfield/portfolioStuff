312eb3ea905097841dbadba6ac2d4f7bc1c63a3c
Ricky Barfield <barfieldfredrick@gmail.com>
2023-09-06 22:17:12 -0700

    JS now all functional! Just gotta make the style sheets cooperate for mobile

--- The Budding Chef/js/main.js
+++ The Budding Chef/js/main.js
@@ -1,4 +1,5 @@
 document.getElementById('ingredientButton').addEventListener('click', getFood)
+document.getElementById('hiddenIngredientButton').addEventListener('click', getFood)
 document.getElementById('recipeButton').addEventListener('click', cookDirections)
 
 function scrollChildCheck () {
@@ -17,13 +18,12 @@ function recipeIngredientChildCheck () {
 
 
 function getFood () {
-    let foodSearch = document.querySelector('input').value
-    console.log(foodSearch)
-    let resultNum = []
+    document.getElementById('hideSection').classList.remove('hidden')
+    document.getElementById('hideSection').scrollIntoView()
+    let foodSearch = document.getElementById('ingredientInput').value
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${foodSearch}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         scrollChildCheck()
         if(data.meals === null) {
             document.getElementById('resultNum').innerText = 'No Items found! Try again with a different food.'
@@ -42,41 +42,69 @@ function getFood () {
     })
 }
 
-// video wont load. needs embed, not watch in link: text = text.replace("watch?v=", "embed/")
 function cookDirections () {
-    let h3 = document.getElementById('recipeTitle')
-    let ul = document.getElementById('recipeIngredients')
-    let p = document.getElementById('recipeDirections')
-
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+    document.getElementById('hideSection').classList.remove('hidden')
+    document.getElementById('hideSection').scrollIntoView()
+    let recipeSearch = document.getElementById('recipeSearch').value
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeSearch}`)
     .then(res => res.json())
     .then(data => {
-        // console.log(data)
         recipeIngredientChildCheck()
-        let youtubeLink = data.meals[0].strYoutube
-        youtubeLink = youtubeLink.replace("watch?v=", "embed/")
-        // console.log(youtubeLink)
-
         document.getElementById('midPic').src = data.meals[0].strMealThumb
         document.getElementById('midLink').href = data.meals[0].strYoutube
         if (data.meals[0] === null) {
             document.h3.innerText = 'Recipe Not Found! Try again.'
         } else {
+            let h3 = document.getElementById('recipeTitle')
+            let ingredientString = ''
+            let ingredientListDOM = document.getElementById('recipeIngredients')
+            let p = document.getElementById('recipeDirections')
+            let recMeal = data.meals[0] 
             h3.innerText = data.meals[0].strMeal
+            
+            ingredientString = 
+            recMeal.strMeasure1 + ' ' + recMeal.strIngredient1 + '. ' + 
+            recMeal.strMeasure2 + ' ' + recMeal.strIngredient2 + '. ' +
+            recMeal.strMeasure3 + ' ' + recMeal.strIngredient3 + '. ' +
+            recMeal.strMeasure4 + ' ' + recMeal.strIngredient4 + '. ' +
+            recMeal.strMeasure5 + ' ' + recMeal.strIngredient5 + '. ' +
+            recMeal.strMeasure6 + ' ' + recMeal.strIngredient6 + '. ' +
+            recMeal.strMeasure7 + ' ' + recMeal.strIngredient7 + '. ' +
+            recMeal.strMeasure8 + ' ' + recMeal.strIngredient8 + '. ' +
+            recMeal.strMeasure9 + ' ' + recMeal.strIngredient9 + '. ' +
+            recMeal.strMeasure10 + ' ' + recMeal.strIngredient10 + '. ' +
+            recMeal.strMeasure11 + ' ' + recMeal.strIngredient11 + '. ' +
+            recMeal.strMeasure12 + ' ' + recMeal.strIngredient12 + '. ' +
+            recMeal.strMeasure13 + ' ' + recMeal.strIngredient13 + '. ' +
+            recMeal.strMeasure14 + ' ' + recMeal.strIngredient14 + '. ' +
+            recMeal.strMeasure15 + ' ' + recMeal.strIngredient15 + '. ' +
+            recMeal.strMeasure16 + ' ' + recMeal.strIngredient16 + '. ' +
+            recMeal.strMeasure17 + ' ' + recMeal.strIngredient17 + '. ' +
+            recMeal.strMeasure18 + ' ' + recMeal.strIngredient18 + '. ' +
+            recMeal.strMeasure19 + ' ' + recMeal.strIngredient19 + '. ' +
+            recMeal.strMeasure20 + ' ' + recMeal.strIngredient20 + '. '
+
+            ingredientString = ingredientString.replace(/null/g, '')
+            ingredientListDOM.innerHTML = ingredientString
+
             p.innerText = data.meals[0].strInstructions
-            console.log(data.meals[0].strIngredient1)
-            for (let i = 0; i < 21; i++) {
-                if (data.meals.strInstructions[i] === null || data.meals.strInstructions[i] === '') {
-                    continue
-                } else{
-                    let li = document.createElement('li')
-                    // this is coming back with undefined. Need to figure out a way to append 1-20 to 'strIngredient' without type conflict...
-                    let ingredient = data.meals[0].strIngredient[String(i)]
-                    li.textContent = ingredient
-                    document.ul.appendChild(li)
-                }
+            
+
+
+            // this is coming back with undefined. Need to figure out a way to append 1-20 to 'strIngredient' without type conflict...Until then, I'm going to do this a much less elegant way.
+
+            // for (let i = 0; i < 21; i++) {
+            //     if (data.meals.strInstructions[i] === null || data.meals.strInstructions[i] === '') {
+            //         continue
+            //     } else{
+            //         let li = document.createElement('li')
+                    
+            //         let ingredient = data.meals[0].strIngredient[String(i)]
+            //         li.textContent = ingredient
+            //         document.ul.appendChild(li)
+            //     }
         
-            }
+            // }
         }
     })
     .catch(err => {
